979000e27ea6fda8c057f804b7fd65c2a7c4c311
Filipe <me@filipefigueiredo.com>
2016-07-17 18:04:07 -0500

    edits

---
+++ library/Gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			dist: {
+				options: {
+					style: 'compressed'
+				},
+				files: {
+					'build/css/main.css': 'src/scss/main.scss'
+				}
+			}
+		},
+		concat: {
+			options: {
+				separator: "\n\n"
+			},
+			css: {
+				src: ['src/css/vendor/*.css'],
+				dest: 'build/css/vendor/plugins.css'
+			},
+			js: {
+				src: ['src/js/vendor/*.js'],
+				dest: 'build/js/vendor/plugins.js'
+			}
+		},
+		uglify: {
+			my_target: {
+				files: {
+					'build/js/main.min.js': ['src/js/main.js']
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: ['src/**/*.scss'],
+				tasks: ['sass']
+			},
+			js: {
+				files: ['src/js/main.js'],
+				tasks: ['uglify']
+			},
+			vendor: {
+				files: ['src/css/vendor/*.css', 'src/js/vendor/*.js', 'src/js/partials/*.js'],
+				tasks: ['concat']
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+};
